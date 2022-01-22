e98320acbfdb55c6216b211e27e308849093d316
loraleep <loraleelu@byui.edu>
2022-01-21 19:07:22 -0700

    message

---
+++ lesson03/scriptr.js
@@ -0,0 +1,8 @@
+
+// toLocaleDateString
+const options = {weekday: 'log', day: 'numeric', month: 'long', year:'numeric'};
+let date = new Date()
+console.log(date)
+document.getElementById('date-time').textContent = date
+
+      const todaysdate = new Date();
