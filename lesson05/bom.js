12e17fbababd194a46a2a38b78bd13908bc70b20
loraleep <loraleelu@byui.edu>
2022-02-08 17:10:40 -0700

    message

--- lesson05/bom.js
+++ lesson05/bom.js
@@ -1,10 +1,26 @@
-const button = button;
-const input = input;
-const list = list;
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-function saveUserData() {
-    var name = document.getElementById('favchap').value;
-    console.log("users value is: " + name);
-    document.getElementById("list").innerHTML = name;
- }
-button.addEventLIstener('click', saveUserData())
+button.addEventListener('click', function() {
+    let myItem = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = '❌';
+    list.appendChild(listItem);
+
+    listBtn.onclick = function(e) {
+      list.removeChild(listItem);
+    }
+
+    input.focus();
+
+
+})
