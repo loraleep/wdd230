d10fc440f51b3891295f49d3a3adc0ebef0e9b9b
loraleep <loraleelu@byui.edu>
2022-02-28 14:18:49 -0700

    message

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -6,3 +6,33 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 640) mainnav.classList.remove('responsive')};
 
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute('data-src')
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions ={};
+
+const imgObserver = new IntersectionObserver((enteries, imgObserver) => {
+    enteries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.terget);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+
